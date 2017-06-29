16b88b09bcfa25697a1a1d12516ea0890edd700d
aidan200 <372944573@qq.com>
2017-06-29 19:53:40 +0800

    anif

---
+++ js/aniJS.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+var oldFtopW;
+var oldFtopf;
+var newfDiv;
+var ff = false;
+$(document).ready(function(){
+    var anifTop = document.getElementsByClassName("anifTopLe")[0];
+    oldFtopf = anifTop.parentNode;
+    oldFtopW = oldFtopf.offsetWidth;
+    var ml = $(oldFtopf).css('paddingLeft');
+    var ss = ml.substring(0,ml.indexOf('px'));
+    newfDiv = anifTop.cloneNode(true);
+    $(newfDiv).addClass('anifTop');
+    $(newfDiv).width(oldFtopW-ss);
+})
+window.onscroll = function () {
+    anflisten();
+}
+
+//滚动后浮动顶部元素
+function anflisten() {
+    var anifTop = document.getElementsByClassName("anifTopLe")[0];
+    var s = anifTop.getBoundingClientRect().top;
+        if(s<=0){
+            if(!ff){
+                oldFtopf.appendChild(newfDiv);
+                ff = true;
+            }
+        }else{
+            if(ff){
+                oldFtopf.removeChild(newfDiv);
+                ff = false;
+            }
+        }
+}
+
+//获取滚动距离
+function ___getPageScroll() {
+    var xScroll, yScroll;
+    if (self.pageYOffset) {
+        yScroll = self.pageYOffset;
+        xScroll = self.pageXOffset;
+    } else if (document.documentElement && document.documentElement.scrollTop) { // Explorer 6 Strict
+        yScroll = document.documentElement.scrollTop;
+        xScroll = document.documentElement.scrollLeft;
+    } else if (document.body) {// all other Explorers
+        yScroll = document.body.scrollTop;
+        xScroll = document.body.scrollLeft;
+    }
+    arrayPageScroll = new Array(xScroll,yScroll);
+    return arrayPageScroll;
+};
