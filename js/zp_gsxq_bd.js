b2dbf6658b576a8f6430141026c35de9f799b331
ysw1966 <ysw1966>
2017-07-04 19:07:15 +0800

    asdasd

--- js/zp_gsxq_bd.js
+++ js/zp_gsxq_bd.js
@@ -4,6 +4,7 @@
 $(function (){
     var width=$(window).width();
     var height=$(window).height();
+    
     $('.zp_gsxq_hy em').on('click',function (){
         $('#zp_gsxq_bd_tck').css({
             "display":"block",
@@ -18,7 +19,18 @@ $(function (){
             "height":"450"
         })
     });
-    $('.zp_gsxq_bd_tck_bottom button').on('click',function (){
+    $('.zp_gsxq_bd_tck_middle a').on("click",function (){//选择行业
+        $('.zp_gsxq_hy > input').val($(this).html())
+        $('#zp_gsxq_bd_tck').css({
+            "display":"none"
+        });
+        $(this).parent().parent().parent().parent().css({
+            "display":"none",
+            "width":"0",
+            "height":"0"
+        })
+    });
+    $('.zp_gsxq_bd_tck_bottom button').on('click',function (){//关闭按钮
         $('#zp_gsxq_bd_tck').css({
             "display":"none"
         });
@@ -28,7 +40,18 @@ $(function (){
             "height":"0"
         })
 
-    })
+    });
+    $('.gb').on("click",function (){//关闭按钮
+        $('#zp_gsxq_bd_tck').css({
+            "display":"none"
+        });
+        $(this).parent().parent().css({
+            "display":"none",
+            "width":"0",
+            "height":"0"
+        })
+    });
+
 //   省市联动开始
     $(".zp_gsxq_dz em").on('click',function (){
         $('#zp_gsxq_bd_tck').css({
@@ -41,7 +64,20 @@ $(function (){
         });
         $('#zp_gsxq_bd_tck_cont2').animate({
             "width":"700",
-            "height":"450"
+            "height":"300"
+        })
+    })
+    $('.zp_gsxq_bd_tck_bottom > a').on("click",function (){
+        var a=$('.zp_gsxq_bd_tck_cont2_cont select').eq(0).find("option:selected").text();
+        var b=$('.zp_gsxq_bd_tck_cont2_cont select').eq(1).find("option:selected").text();
+        $('.zp_gsxq_dz > input').eq(0).val(a+"/"+b);
+        $('#zp_gsxq_bd_tck').css({
+            "display":"none"
+        });
+        $(this).parent().parent().css({
+            "display":"none",
+            "width":"0",
+            "height":"0"
         })
     })
 });
